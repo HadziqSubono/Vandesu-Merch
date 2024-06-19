b15cd682d4ac1422c5e26a9f12928e7d3ec7e0c5
HadziqSubono <mhadziqsoebono42@gmail.com>
2024-06-19 22:29:05 +0700

    update vandesu merch

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $queryKategori = mysqli_query($con, "SELECT id, nama FROM kategori");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Toko Vandesu | Home</title>
+    <title>Vandesu Merch | Home</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
